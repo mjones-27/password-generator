22873a29c18ef2c9dc6ffaca3db85e8dca3e1a9b
unknown <mars.jones34@gmail.com>
2020-03-12 21:19:50 -0700

    running code

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var number = "0123456789";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = " !$%^&*()-=+[]{};#:@~,./<>?";
+var numberArr = number.split("");
+var lowerArr = lower.split("");
+var upperArr = upper.split("");
+var specialArr = special.split("");
+// TODO: force password length check - and maybe make sure it's a number?
+var pwdLength = prompt("Enter desired password length (between 8 and 128 characters") * 1;
+var specialChars = confirm("Do you want your password to have special characters? (e.g. #$%^&)");
+var lowerChars = confirm("Do you want your password to have lower case characters?");
+var upperChars = confirm("Do you want your password to have upper case characters?");
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
+  var password = "12345";
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
